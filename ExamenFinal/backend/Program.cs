b9abd2cb42b52cdb1402aef100828ac53eb39dbf
MarioCordero <mariogabriel.cordero@uacr.ac.cr>
2025-12-06 14:48:54 -0600

    REFRACT: Que el backend admita CORS

--- ExamenFinal/backend/Program.cs
+++ ExamenFinal/backend/Program.cs
@@ -8,6 +8,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 builder.Services.AddSingleton<DatabaseMostra>();
 builder.Services.AddScoped<ICoffeeService, CoffeeService>();
 builder.Services.AddScoped<ICoffeeRepository, CoffeeRepository>();
@@ -26,6 +36,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowFrontend");
 
 app.UseAuthorization();
 
