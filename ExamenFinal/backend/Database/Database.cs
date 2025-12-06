f668b80a572f431861eb12d75dc93065779ac9a2
MarioCordero <mariogabriel.cordero@uacr.ac.cr>
2025-12-06 07:17:11 -0600

    REFRACT: Cambiando valores de la base de datos por datos significativos en los diccionarios

--- ExamenFinal/backend/Database/Database.cs
+++ ExamenFinal/backend/Database/Database.cs
@@ -2,29 +2,30 @@
 {
     public class DatabaseMostra
     {
-        public Dictionary<string, int> keyValues = new Dictionary<string, int>
+        public Dictionary<string, int> CoffeeInventory = new Dictionary<string, int>
         {
             { "Americano", 10 },
             { "Cappuccino", 8 },
             { "Lates", 10 },
-            { "Mocaccino", 15}
+            { "Mocaccino", 15 }
         };
 
-        public Dictionary<string, int> keyValues2 = new Dictionary<string, int>
+        public Dictionary<string, int> CoffeePrices = new Dictionary<string, int>
         {
             { "Americano", 950 },
             { "Cappuccino", 1200 },
             { "Lates", 1350 },
-            { "Mocaccino", 1500}
+            { "Mocaccino", 1500 }
         };
 
-        public Dictionary<int, int> keyValues3 = new Dictionary<int, int>
+        public Dictionary<int, int> CoinInventory = new Dictionary<int, int>
         {
             { 500, 20 },
             { 100, 30 },
             { 50, 50 },
-            { 25, 25}
+            { 25, 25 }
         };
-        public List<int> SortedDenominations { get; } = new List<int> { 500, 100, 50, 25 }; // The coins sorted in descending order
+
+        public List<int> SortedDenominations { get; } = new List<int> { 500, 100, 50, 25 };
     }
 }
