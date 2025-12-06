a9c0abb49ccf61aee00244c6e22868c2527a6d45
MarioCordero <mariogabriel.cordero@uacr.ac.cr>
2025-12-06 15:15:16 -0600

    FEAT: Modelo para poder manejar el inventario de monedas/billetes de la maquina

--- ExamenFinal/backend/Models/PaymentDenominations.cs
+++ ExamenFinal/backend/Models/PaymentDenominations.cs
@@ -2,7 +2,7 @@ namespace ExamTwo.Models
 {
     public class PaymentDenominations
     {
-        public Dictionary<int, int> Coins { get; set; }  // { 1000: 5, 500: 10, 100: 20, 50: 30, 25: 15 }
-        public Dictionary<int, int> Bills { get; set; }   // { 1000: 3, 500: 2 }
+        public Dictionary<int, int> Coins { get; set; }
+        public Dictionary<int, int> Bills { get; set; }
     }
 }
